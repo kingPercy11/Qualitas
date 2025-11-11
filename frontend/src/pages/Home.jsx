25b8e4c688639437dc56a48601d16d1aebe5e05b
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-11-11 18:52:41 +0530

    Logo edit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -29,13 +29,21 @@ const Home = () => {
   ];
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-linear-to-br from-gray-100 via-blue-50 to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 text-gray-900 dark:text-gray-100 transition-all">
-      <h1 className="text-4xl font-bold mb-4"> Hi there!</h1>
+    <div className="min-h-screen flex flex-col bg-linear-to-br from-gray-100 via-blue-50 to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 text-gray-900 dark:text-gray-100 transition-all">
+      {/* Nav / Header */}
+      <nav className="w-full bg-white-800 opacity-90 py-4 px-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <img src="/Logo.png" alt="Qualitas" className="h-15 w-auto" />
+        </div>
+      </nav>
+
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold mb-4"> Hi there!</h1>
       <p className="text-lg mb-10 text-gray-700 dark:text-gray-300">
         What would you like to do today?
       </p>
 
-      {/* Options Grid */}
+  {/* Options Grid */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-11/12 md:w-4/5 lg:w-3/5">
         {cards.map((card, index) => (
           <div
@@ -49,6 +57,7 @@ const Home = () => {
           </div>
         ))}
       </div>
+    </div>
     </div>
   );
 };
